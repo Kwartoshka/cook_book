d67a60a65fca62d3d9b54efd0a334ee3c00fc119
Andrey Zaytsev <and.zaytsev9@gmail.com>
2020-11-27 15:53:57 +0300

    Task 2 is done

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        for ingridient in cook_book[dish]:
+            if ingridient['ingridient_name'] not in shop_list:
+                shop_list[ingridient['ingridient_name']] = {
+                    'quantity': ingridient['quantity'] * person_count,
+                    'measure': ingridient['measure']
+                }
+            else:
+                quantity = ingridient['quantity']
+                (shop_list[ingridient['ingridient_name']]['quantity']) += ingridient['quantity'] * person_count
+    return shop_list
+
+
+
 cook_book = {}
 
 with open('recipes.txt', encoding='utf-8') as recipes:
     recipes_list = recipes.read().splitlines()
 
-# for line in recipes_list:
-#     line = line.strip()
 next_line = 0
 for index in range(len(recipes_list)):
     if index != next_line:
@@ -13,9 +27,14 @@ for index in range(len(recipes_list)):
     current_list = []
     for ingrid in range(lines):
         current_dict = {}
-        current_dict['ingridient_name'], current_dict['quantity'], current_dict['measure'] = recipes_list[index + 2 + ingrid].split(' | ')
+        current_dict['ingridient_name'], current_dict['quantity'], current_dict['measure'] = \
+            recipes_list[index + 2 + ingrid].split(' | ')
+        current_dict['quantity'] = int(current_dict['quantity'])
         current_list.append(current_dict)
     next_line = index + 3 + lines
     cook_book[recipes_list[index]] = current_list
 
 # print(cook_book)
+get_shop_list_by_dishes(['Омлет', 'Утка по-пекински'], 1)
+# print(get_shop_list_by_dishes(['Омлет', 'Утка по-пекински'], 2))
+
